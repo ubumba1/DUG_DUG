2369a840ef05a9796668cbe9d7117482c0752026
ubumba1 <maxslaz@gmail.com>
2023-10-31 23:57:50 +0300

    bacground_new

--- Assets/scripts/moveBackground.cs
+++ Assets/scripts/moveBackground.cs
@@ -1,33 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class moveBackground : MonoBehaviour
 {
-    [SerializeField] 
+    [SerializeField]
     float _speed;
     private Transform back_Transform;
     private float back_Size;
     private float back_pos;
 
-    
+    private float targetBackPos; 
 
     void Start()
     {
         back_Transform = GetComponent<Transform>();
         back_Size = GetComponent<SpriteRenderer>().bounds.size.y;
-
+        targetBackPos = back_pos; 
     }
 
-   
-
-
-
-    public void RaiseBackground(float amount)
+    void Update()
     {
-        back_pos += amount; 
+        Move();
+    }
+
+    public void Move()
+    {
+        back_pos = Mathf.Lerp(back_pos, targetBackPos, _speed * Time.deltaTime); 
+
         back_pos = Mathf.Repeat(back_pos, back_Size);
         back_Transform.position = new Vector3(0, back_pos, 0);
     }
 
+    public void RaiseBackground(float amount)
+    {
+        targetBackPos += amount; 
+    }
 }
